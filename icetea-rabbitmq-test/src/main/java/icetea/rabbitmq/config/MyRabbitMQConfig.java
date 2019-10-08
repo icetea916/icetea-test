35849257e2b26502cf4bf1af2b4efda8e28042b3
icetea <icetea916@aliyun.com>
2019-10-08 16:59:21 +0800

    @feature 配置消费者

--- icetea-rabbitmq-test/src/main/java/icetea/rabbitmq/config/MyRabbitMQConfig.java
+++ icetea-rabbitmq-test/src/main/java/icetea/rabbitmq/config/MyRabbitMQConfig.java
@@ -4,9 +4,6 @@ import icetea.rabbitmq.constant.RabbitMqConstant;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
@@ -32,13 +29,4 @@ public class MyRabbitMQConfig {
     public MessageConverter messageConverter() {
         return new Jackson2JsonMessageConverter();
     }
-
-    @Bean("rabbitListenerContainerFactory")
-    public RabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
-        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        factory.setConnectionFactory(connectionFactory);
-        factory.setMessageConverter(new Jackson2JsonMessageConverter());
-        return factory;
-    }
-
 }
