682e46da8afe9c8ba665d1faa8ea427d83608ed9
icetea <icetea916@aliyun.com>
2019-10-09 23:10:09 +0800

    spring-cloud-eureka 学习

---
+++ spring-cloud-eureka-client-one/src/main/java/icetea/spring/cloud/eureka/client/EurekaClientOne.java
@@ -0,0 +1,13 @@
+package icetea.spring.cloud.eureka.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientOne {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientOne.class);
+    }
+}
---
+++ spring-cloud-eureka-client-one/src/main/java/icetea/spring/cloud/eureka/client/controller/ClientOneController.java
@@ -0,0 +1,13 @@
+package icetea.spring.cloud.eureka.client.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ClientOneController {
+
+    @GetMapping("test")
+    public String test() {
+        return "ok";
+    }
+}
