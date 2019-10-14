c9a3b6a2ec5d4d7d29b7f3485febf56eeb76f7af
icetea <icetea916@aliyun.com>
2019-10-14 18:19:38 +0800

    @feature 增加pc端观看记录压测

--- icetea-rabbitmq-test/src/main/java/icetea/rabbitmq/test/TestPublisherController.java
+++ icetea-rabbitmq-test/src/main/java/icetea/rabbitmq/test/TestPublisherController.java
@@ -40,4 +40,72 @@ public class TestPublisherController {
         return "OK";
     }
 
+    /**
+     * 压力测试is_pass队列
+     */
+    @GetMapping("isPass/{number}")
+    public String publishIsPassMessage(@PathVariable Integer number) {
+        // 消息构造
+        JSONObject messageMap = new JSONObject();
+
+        int i = 0;
+        while (i < number) {
+            for (int j = 2650501; j <= 2650536; j++) {
+                messageMap.put("upid", j);
+                rabbitTemplate.convertAndSend("zjtx.topic", "zjtx.plan.ispass.pc", messageMap);
+            }
+
+            i++;
+        }
+
+        return "OK";
+    }
+
+    /**
+     * 压力测试pc观看记录
+     *
+     * @return
+     */
+    @GetMapping("pc/{number}")
+    public String publishPcMessage(@PathVariable Integer number) {
+        // {"errcode":200,
+        // "upid":2650539,
+        // "lookTime":548,
+        // "csid":5219,
+        // "ip":"111.204.5.6",
+        // "sessionId":"ff7597a5-930d-448f-9ccc-c26ff70133e4",
+        // "userid":2098569,
+        // "token":"e1ed9ee6c10f7bce63a5c3feb02c2c91",
+        // "startTime2":1571041528271,
+        // "watchTime":8,
+        // "startTime":1571041520302,
+        // "allowTime":900,
+        // "time":0,
+        // "endTime":1571041529693,
+        // "cid":2435}
+        JSONObject messageMap = new JSONObject();
+        messageMap.put("errcode", 200);
+        messageMap.put("upid", 2650539);
+        messageMap.put("lookTime", 548);
+        messageMap.put("csid", 5219);
+        messageMap.put("ip", "188.188.188.188");
+        messageMap.put("sessionId", "ff7597a5-930d-448f-9ccc-c26ff70133e4");
+        messageMap.put("userid", 2098569);
+        messageMap.put("token", "e1ed9ee6c10f7bce63a5c3feb02c2c91");
+        messageMap.put("startTime2", 1571041528271L);
+        messageMap.put("watchTime", 8);
+        messageMap.put("startTime", 1571041520302L);
+        messageMap.put("allowTime", 900);
+        messageMap.put("time", 0);
+        messageMap.put("endTime", 1571041529693L);
+        messageMap.put("cid", 2435);
+
+        for (int j = 0; j < number; j++) {
+            rabbitTemplate.convertAndSend("zjtx.topic", "zjtx.looktime.pc", messageMap);
+        }
+
+
+        return "ok";
+
+    }
 }
---
+++ icetea-rabbitmq-test/src/test/java/icetea/rabbitmq/SimpleTest.java
@@ -0,0 +1,14 @@
+package icetea.rabbitmq;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+public class SimpleTest {
+
+    @Test
+    public void simple() {
+        Date date = new Date();
+        System.out.println(date);
+    }
+}
