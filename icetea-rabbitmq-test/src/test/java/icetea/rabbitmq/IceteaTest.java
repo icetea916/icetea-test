45362714b7f39a39983394144c925d7028d3a028
icetea <icetea916@aliyun.com>
2019-10-18 16:14:22 +0800

    @feature 增加pc端观看记录压测

--- icetea-rabbitmq-test/src/test/java/icetea/rabbitmq/IceteaTest.java
+++ icetea-rabbitmq-test/src/test/java/icetea/rabbitmq/IceteaTest.java
@@ -3,11 +3,41 @@ package icetea.rabbitmq;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @SpringBootTest
 public class IceteaTest {
 
     @Test
     public void test1() {
         System.out.println("12312321");
+    }
+
+    @Test
+    public void test2() {
+        Map<String, Object> map1 = new HashMap<>();
+        map1.put("username", "<p>亲爱的专技天下网学员您们好！</p><p>&nbsp;&nbsp;&nbsp; 为给广大学员提供更优质的学习体验，专技天下网于2019.6.22正式上线最新版，如有任何良好建议，请您随时向我们提出，企业邮箱：111@zgzjzj.com，您的建议采纳后会第一时间进行优化，感谢您的支持！<br></p>");
+        map1.put("age", 1);
+        List<Map<String, Object>> list = new ArrayList<>();
+
+        list.add(map1);
+
+        System.out.println(list);
+        System.out.println("=====================");
+        // 测试foreach
+        list.forEach(map -> {
+            String content = (String) map.get("username");
+            // 剔除html标签
+            content = content.replaceAll("</?[^>]+>", "").replaceAll("&nbsp;", "");
+            // 截取70个字符
+            content = content.substring(0, 70);
+            // 更新字符串
+            map.put("username", content);
+        });
+
+        System.out.println(list);
     }
 }
