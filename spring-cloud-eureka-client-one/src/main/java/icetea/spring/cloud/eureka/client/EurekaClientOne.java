10ce13985386d90393ab4c1ed2eb984e602ea265
icetea <icetea916@aliyun.com>
2019-10-24 21:26:56 +0800

    示例：使用带ribbon的restTemplate进行服务调用

--- spring-cloud-eureka-client-one/src/main/java/icetea/spring/cloud/eureka/client/EurekaClientOne.java
+++ spring-cloud-eureka-client-one/src/main/java/icetea/spring/cloud/eureka/client/EurekaClientOne.java
@@ -3,7 +3,10 @@ package icetea.spring.cloud.eureka.client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,5 +14,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class EurekaClientOne {
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientOne.class);
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
     }
 }
--- spring-cloud-eureka-client-one/src/main/java/icetea/spring/cloud/eureka/client/controller/ClientOneController.java
+++ spring-cloud-eureka-client-one/src/main/java/icetea/spring/cloud/eureka/client/controller/ClientOneController.java
@@ -1,5 +1,6 @@
 package icetea.spring.cloud.eureka.client.controller;
 
+import icetea.spring.cloud.eureka.client.discoveryClient.LoadBalanceClient;
 import icetea.spring.cloud.eureka.client.discoveryClient.TwoDiscoveryClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +11,17 @@ public class ClientOneController {
 
     @Autowired
     TwoDiscoveryClient twoDiscoveryClient;
+    @Autowired
+    private LoadBalanceClient loadBalanceClient;
 
     @GetMapping("test")
     public String test() {
         return twoDiscoveryClient.test1();
     }
+
+    @GetMapping("test2")
+    public String test2() {
+        return loadBalanceClient.test1();
+    }
+
 }
