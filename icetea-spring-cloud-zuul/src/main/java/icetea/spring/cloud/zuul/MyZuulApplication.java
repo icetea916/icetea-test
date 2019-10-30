e6b83ec7999d4ca1b0d8a01363846c8b9caca182
icetea <icetea916@aliyun.com>
2019-10-30 08:31:17 +0800

    @feature zuul配置

---
+++ icetea-spring-cloud-zuul/src/main/java/icetea/spring/cloud/zuul/MyZuulApplication.java
@@ -0,0 +1,13 @@
+package icetea.spring.cloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class MyZuulApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyZuulApplication.class);
+    }
+}
