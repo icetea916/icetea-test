8f2932326d5c4b7b703b17d6455aa605201771e5
icetea <icetea916@aliyun.com>
2019-10-31 14:53:56 +0800

    @学习  学习IO

---
+++ learn-io/src/main/java/icetea/test/io/MyIoApplication.java
@@ -0,0 +1,13 @@
+package icetea.test.io;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyIoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyIoApplication.class);
+    }
+    
+}
