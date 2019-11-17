6a8d180dc570cb887f976c9e0b96e79997450585
icetea <icetea916@aliyun.com>
2019-11-17 20:52:40 +0800

    @learning 学习zuul的搭建

--- icetea-spring-cloud-zuul/src/main/java/icetea/spring/cloud/zuul/MyZuulApplication.java
+++ icetea-spring-cloud-zuul/src/main/java/icetea/spring/cloud/zuul/MyZuulApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+// 若想使用eureka必须导入spring Cloud的eureka包
 @EnableZuulProxy
 public class MyZuulApplication {
     public static void main(String[] args) {
