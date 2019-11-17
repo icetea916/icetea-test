083247c5bb60813ea363100c4aa1fd9b27fb4f16
icetea <icetea916@aliyun.com>
2019-11-17 15:40:40 +0800

    learning hystrixCommand注解

---
+++ spring-cloud-eureka-client-one/src/main/java/icetea/spring/cloud/eureka/client/hystrix/TestHystrix.java
@@ -0,0 +1,41 @@
+package icetea.spring.cloud.eureka.client.hystrix;
+
+import com.netflix.hystrix.exception.HystrixRuntimeException;
+import icetea.spring.cloud.eureka.client.hystrix.service.TestHystrixService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("hystrix")
+public class TestHystrix {
+
+    @Autowired
+    private TestHystrixService testHystrixService;
+
+    @RequestMapping("test1")
+    public String testHystrix() {
+        List<String> list = testHystrixService.getList();
+
+        return list.toString();
+    }
+
+    @RequestMapping("testRandomSleep")
+    public String testRandomHystrix() {
+        List<String> list = new ArrayList<>();
+        try {
+            list = testHystrixService.randomGetList();
+        } catch (HystrixRuntimeException e) {
+            System.out.println("hystrix调用超时");
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            System.out.println("线程中断异常");
+        }
+
+        return list.toString();
+    }
+
+}
---
+++ spring-cloud-eureka-client-one/src/main/java/icetea/spring/cloud/eureka/client/hystrix/service/TestHystrixService.java
@@ -0,0 +1,60 @@
+package icetea.spring.cloud.eureka.client.hystrix.service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Service
+public class TestHystrixService {
+
+    @HystrixCommand
+    public List<String> getList() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("李晓烨");
+        list.add("我爱你");
+
+        return list;
+    }
+
+    @HystrixCommand(
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "5000")
+            },
+            fallbackMethod = "fallBackMethod",
+            threadPoolKey = "test1",
+            threadPoolProperties = {
+                    @HystrixProperty(name = "coreSize", value = "30"),
+                    @HystrixProperty(name = "maxQueueSize", value = "10")
+            }
+    )
+    public List<String> randomGetList() throws InterruptedException {
+        Random random = new Random();
+
+        int randomNum = random.nextInt(3) + 1;
+
+        if (randomNum == 3) {
+            Thread.sleep(11000);
+        }
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("我爱你");
+        list.add("李晓烨");
+
+        return list;
+    }
+
+
+    public List<String> fallBackMethod() {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("我爱你");
+        list.add("李晓烨");
+        list.add("forever");
+
+        return list;
+    }
+
+}
