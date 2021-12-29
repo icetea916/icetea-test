fc19d8b34865c26e0aaa7c962b6a0447410c806e
icetea <icetea916@aliyun.com>
2021-12-29 14:17:44 +0800

    test

--- java8-lambada/src/main/java/life/icetea/test/lambda/Test.java
+++ java8-lambada/src/main/java/life/icetea/test/lambda/Test.java
@@ -7,7 +7,7 @@ public class Test {
     public static void main(String[] args) {
         Button button = new Button();
         button.addActionListener((event) -> {
-            System.out.println("button clicked");
+            System.out.println("button clicked icetea");
         });
     }
 
