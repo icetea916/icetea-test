1eec10a4be90febe9a059b9040f7a892035b6f50
icetea <icetea916@163.com>
2025-06-08 17:37:25 +0800

    test spring init

---
+++ test-spring/src/main/java/life/icetea/test/spring/beanpostprocessor/B.java
@@ -0,0 +1,7 @@
+package life.icetea.test.spring.beanpostprocessor;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class B {
+}
--- test-spring/src/main/java/life/icetea/test/spring/beanpostprocessor/MyBeanPostProcessor.java
+++ test-spring/src/main/java/life/icetea/test/spring/beanpostprocessor/MyBeanPostProcessor.java
@@ -2,10 +2,12 @@ package life.icetea.test.spring.beanpostprocessor;
 
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
 
 /**
  * 定义一个BeanPostProcessor
  */
+@Component
 public class MyBeanPostProcessor implements BeanPostProcessor {
 
     /**
